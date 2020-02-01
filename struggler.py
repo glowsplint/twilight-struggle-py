4758bb9daee91c9f4ff66498f6a8ed75fcf5c625
Tym Teo <tym.teo.23@gmail.com>
2020-02-01 17:55:47 +0800

    added additional functionality for early war cards

--- struggler.py
+++ struggler.py
@@ -3,22 +3,23 @@
 
 # ### Twilight Struggle
 
-# In[ ]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 import random
 from twilight_map import *
+from twilight_cards import *
 
 
-# In[ ]:
+# In[2]:
 
 
 all_countries = dict()
 
 
-# In[ ]:
+# In[3]:
 
 
 class country:
@@ -142,7 +143,7 @@ class country:
         print(f'US rolled: {us_roll}, USSR rolled: {ussr_roll}, Modifer = {modifier}, Difference = {difference}')
 
 
-# In[ ]:
+# In[4]:
 
 
 ''' Creates entire map. '''
@@ -235,9 +236,9 @@ Venezuela = country(**Venezuela)
 Chinese_Civil_War = country(**Chinese_Civil_War)
 
 
-# ### Recreating coup and realignment logic.
+# ### Build standard map
 
-# In[ ]:
+# In[5]:
 
 
 def build_standard_map():
@@ -259,7 +260,7 @@ def build_standard_map():
     South_Africa.set_influence(1, 0)
 
 
-# In[ ]:
+# In[6]:
 
 
 build_standard_map()
@@ -267,7 +268,7 @@ build_standard_map()
 
 # ### Create cards, hand, discard pile, draw pile, removed pile
 
-# In[ ]:
+# In[7]:
 
 
 class card:
@@ -280,16 +281,21 @@ class card:
     5. Space race
     6. Trigger event first >> realignment/coup/influence
     '''
-    def __init__(self, card_number):
+    def __init__(self, card_number, **kwargs):
         self.card_number = card_number
+        for key, value in kwargs.items():
+            setattr(self, key, value)
     
     def __repr__(self):
-        return f'card({self.card_number})'
+        sb = []
+        for key in self.__dict__:
+            sb.append("{key}='{value}'".format(key=key, value=self.__dict__[key]))
+        return ', '.join(sb)
 
 
 # Let's create cards by index 1-110. Early war: 1-35, 103-106. Mid war: 36-81, 107-108. Late war: 82-102, 109-110. China card is card(6).
 
-# In[ ]:
+# In[8]:
 
 
 all_cards = [card(i+1) for i in range(110)]
@@ -301,7 +307,7 @@ late_war_indices = [i+1 for i in range(82-1,102)]
 late_war_indices.extend([i+1 for i in range(109-1,110)])
 
 
-# In[ ]:
+# In[9]:
 
 
 early_war_cards = [card(i) for i in early_war_indices]
@@ -309,7 +315,7 @@ mid_war_cards = [card(i) for i in mid_war_indices]
 late_war_cards = [card(i) for i in late_war_indices]
 
 
-# In[ ]:
+# In[10]:
 
 
 us_hand = []
@@ -317,6 +323,50 @@ ussr_hand = []
 
 
 # ### Setup
+
+# In[11]:
+
+
+Arab_Israeli_War = card(**Arab_Israeli_War)
+Asia_Scoring = card(**Asia_Scoring)
+Blockade = card(**Blockade)
+CIA_Created = card(**CIA_Created)
+COMECON = card(**COMECON)
+Captured_Nazi_Scientist = card(**Captured_Nazi_Scientist)
+Containment = card(**Containment)
+De_Gaulle_Leads_France = card(**De_Gaulle_Leads_France)
+De_Stalinization = card(**De_Stalinization)
+Decolonization = card(**Decolonization)
+Defectors = card(**Defectors)
+Duck_and_Cover = card(**Duck_and_Cover)
+East_European_Unrest = card(**East_European_Unrest)
+Europe_Scoring = card(**Europe_Scoring)
+Fidel = card(**Fidel)
+Five_Year_Plan = card(**Five_Year_Plan)
+Formosan_Resolution = card(**Formosan_Resolution)
+Independent_Reds = card(**Independent_Reds)
+Indo_Pakistani_War = card(**Indo_Pakistani_War)
+Korean_War = card(**Korean_War)
+Marshall_Plan = card(**Marshall_Plan)
+Middle_East_Scoring = card(**Middle_East_Scoring)
+NATO = card(**NATO)
+NORAD = card(**NORAD)
+Nasser = card(**Nasser)
+Nuclear_Test_Ban = card(**Nuclear_Test_Ban)
+Olympic_Games = card(**Olympic_Games)
+Red_Scare_Purge = card(**Red_Scare_Purge)
+Romanian_Abdication = card(**Romanian_Abdication)
+Socialist_Governments = card(**Socialist_Governments)
+Special_Relationship = card(**Special_Relationship)
+Suez_Crisis = card(**Suez_Crisis)
+The_Cambridge_Five = card(**The_Cambridge_Five)
+The_China_Card = card(**The_China_Card)
+Truman_Doctrine = card(**Truman_Doctrine)
+UN_Intervention = card(**UN_Intervention)
+US_Japan_Mutual_Defense_Pact = card(**US_Japan_Mutual_Defense_Pact)
+Vietnam_Revolts = card(**Vietnam_Revolts)
+Warsaw_Pact_Formed = card(**Warsaw_Pact_Formed)
+
 
 # In[ ]:
 
@@ -347,12 +397,6 @@ early_war_cards
 
 
 late_war_cards
-
-
-# In[ ]:
-
-
-
 
 
 # ### Create the turn order
@@ -426,11 +470,12 @@ def space(side):
         print(f'Failure with roll of {roll}.')
 
 
+# ### CA Scoring
+
 # In[ ]:
 
 
 # CA SCORING TEST
-
 def scoring_CA():
     global vp_track
     
