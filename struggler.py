12818b40f8a3c5f765982bc7ab8d0f865658e441
Tym Teo <tym.teo.23@gmail.com>
2020-02-03 09:21:55 +0800

    moved scoring functions to scoring_functionality.py

--- struggler.py
+++ struggler.py
@@ -11,264 +11,12 @@ import numpy as np
 import random
 from twilight_map import *
 from twilight_cards import *
-
-
-# In[2]:
-
-
-all_countries = dict()
-
-
-# In[3]:
-
-
-class country:
-    def __init__(self, country_name, country_index, adjacent_countries, us_influence, ussr_influence, region=0, stability=0, battleground=False, superpower=False, chinese_civil_war=False):
-        self.country_name = country_name
-        self.country_index = country_index
-        self.region = region
-        self.stability = stability
-        self.battleground = battleground
-        self.adjacent_countries = adjacent_countries
-        self.us_influence = us_influence
-        self.ussr_influence = ussr_influence
-        self.superpower = superpower
-        self.chinese_civil_war = chinese_civil_war
-        self.evaluate_control()
-        all_countries[country_name] = self
-
-    def evaluate_control(self):
-        if self.us_influence - self.ussr_influence >= self.stability:
-            self.control = 'us'
-        elif self.ussr_influence - self.us_influence >= self.stability:
-            self.control = 'ussr'
-        else:
-            self.control = 'none'
-
-    def __repr__(self):
-        if self.stability == 0:
-            return f'country({self.country_name}, adjacent = {self.adjacent_countries})'
-        else:
-            return f'country({self.country_name}, region = {self.region}, stability = {self.stability}, battleground = {self.battleground}, adjacent = {self.adjacent_countries}, us_inf = {self.us_influence}, ussr_inf = {self.ussr_influence}), control = {self.control}'
-
-    def set_influence(self, us_influence, ussr_influence):
-        if self.superpower == True:
-            raise ValueError('Cannot set influence on superpower!')
-        self.us_influence = us_influence
-        self.ussr_influence = ussr_influence
-        self.evaluate_control()
-
-    def reset_influence(self):
-        self.us_influence, self.ussr_influence = 0, 0
-        self.evaluate_control()
-
-    def adjust_influence(self, us_influence, ussr_influence):
-        self.us_influence += us_influence
-        self.ussr_influence += ussr_influence
-        self.evaluate_control()
-
-    def place_influence(self, side, effective_operations_points):
-        if side == 'ussr' and self.control == 'us':
-            # here we deduct 2 from effective_operations_points, to place 1 influence in the country, and then call the function again
-            if effective_operations_points >= 2:
-                self.adjust_influence(0, 1)
-            else:
-                raise ValueError('Not enough operations points!')
-            if effective_operations_points - 2 > 0:
-                self.place_influence(side, effective_operations_points - 2)
-        elif side == 'us' and self.control == 'ussr':
-            if effective_operations_points >= 2:
-                self.adjust_influence(1, 0)
-            else:
-                raise ValueError('Not enough operations points!')
-            if effective_operations_points - 2 > 0:
-                self.place_influence(side, effective_operations_points - 2)
-        else:
-            if side == 'us':
-                self.adjust_influence(effective_operations_points, 0)
-            elif side == 'ussr':
-                self.adjust_influence(0, effective_operations_points)
-            else:
-                raise ValueError("side must be 'us' or 'ussr'!")
-
-    def coup(self, side, effective_operations_points):
-        '''
-        TODO:
-        1. Prevent coup if no opposing influence in the country. I would prefer to write this in a way that prevents this from happening altogether, as opposed to throwing up an error if this is tried.
-        2. Prevent coup under DEFCON restrictions. Will write it in a style same as 1.
-        3. Prevent coup if there is no influence at all in the country.
-        4. Add military operations points.
-        5. Reduce DEFCON status level if self.battleground = True
-        '''
-
-        die_roll = np.random.randint(6) + 1
-        difference = die_roll + effective_operations_points - self.stability*2
-        if difference > 0:
-            if side == 'us':
-                # subtract from opposing first.. and then add to yours
-                if difference > self.ussr_influence:
-                    self.adjust_influence(difference - self.ussr_influence, 0)
-                self.adjust_influence(0, -min(difference, self.us_influence))
-
-            if side == 'ussr':
-                if difference > self.us_influence:
-                    self.adjust_influence(0, difference - self.us_influence)
-                self.adjust_influence(-min(difference, self.us_influence), 0)
-            print(f'Coup successful with roll of {die_roll}. Difference: {difference}')
-        else:
-            print(f'Coup failed with roll of {die_roll}')
-        self.evaluate_control()
-    
-    def realignment(self):
-        '''
-        TODO:
-        1. Prevent realignment under DEFCON restrictions.
-        2. Prevent realignment if there is no influence at all in the country.
-        '''
-        
-        modifier = 0
-        for adjacent_country in self.adjacent_countries:
-            modifier += ((all_countries[adjacent_country.country_name]).control == 'us')
-            modifier -= ((all_countries[adjacent_country.country_name]).control == 'ussr')
-        if self.us_influence - self.ussr_influence > 0:
-            modifier += 1
-        elif self.us_influence - self.ussr_influence < 0:
-            modifier -= 1
-        us_roll, ussr_roll = np.random.randint(6) + 1, np.random.randint(6) + 1
-        difference = us_roll - ussr_roll + modifier
-        if difference > 0:
-            self.adjust_influence(0, -min(difference, self.ussr_influence))
-        elif difference < 0:
-            self.adjust_influence(-min(-difference, self.us_influence), 0)
-        print(f'US rolled: {us_roll}, USSR rolled: {ussr_roll}, Modifer = {modifier}, Difference = {difference}')
-
-
-# In[4]:
-
-
-''' Creates entire map. '''
-USSR = country(**USSR)
-USA = country(**USA)
-Canada = country(**Canada)
-UK = country(**UK)
-Norway = country(**Norway)
-Sweden = country(**Sweden)
-Finland = country(**Finland)
-Denmark = country(**Denmark)
-Benelux = country(**Benelux)
-France = country(**France)
-Spain_Portugal = country(**Spain_Portugal)
-Italy = country(**Italy)
-Greece = country(**Greece)
-Austria = country(**Austria)
-West_Germany = country(**West_Germany)
-East_Germany = country(**East_Germany)
-Poland = country(**Poland)
-Czechoslovakia = country(**Czechoslovakia)
-Hungary = country(**Hungary)
-Yugoslavia = country(**Yugoslavia)
-Romania = country(**Romania)
-Bulgaria = country(**Bulgaria)
-Turkey = country(**Turkey)
-Libya = country(**Libya)
-Egypt = country(**Egypt)
-Israel = country(**Israel)
-Lebanon = country(**Lebanon)
-Syria = country(**Syria)
-Iraq = country(**Iraq)
-Iran = country(**Iran)
-Jordan = country(**Jordan)
-Gulf_States = country(**Gulf_States)
-Saudi_Arabia = country(**Saudi_Arabia)
-Afghanistan = country(**Afghanistan)
-Pakistan = country(**Pakistan)
-India = country(**India)
-Burma = country(**Burma)
-Laos_Cambodia = country(**Laos_Cambodia)
-Thailand = country(**Thailand)
-Vietnam = country(**Vietnam)
-Malaysia = country(**Malaysia)
-Australia = country(**Australia)
-Indonesia = country(**Indonesia)
-Philippines = country(**Philippines)
-Japan = country(**Japan)
-Taiwan = country(**Taiwan)
-South_Korea = country(**South_Korea)
-North_Korea = country(**North_Korea)
-Algeria = country(**Algeria)
-Morocco = country(**Morocco)
-Tunisia = country(**Tunisia)
-West_African_States = country(**West_African_States)
-Ivory_Coast = country(**Ivory_Coast)
-Saharan_States = country(**Saharan_States)
-Nigeria = country(**Nigeria)
-Cameroon = country(**Cameroon)
-Zaire = country(**Zaire)
-Angola = country(**Angola)
-South_Africa = country(**South_Africa)
-Botswana = country(**Botswana)
-Zimbabwe = country(**Zimbabwe)
-SE_African_States = country(**SE_African_States)
-Kenya = country(**Kenya)
-Somalia = country(**Somalia)
-Ethiopia = country(**Ethiopia)
-Sudan = country(**Sudan)
-Mexico = country(**Mexico)
-Guatemala = country(**Guatemala)
-El_Salvador = country(**El_Salvador)
-Honduras = country(**Honduras)
-Costa_Rica = country(**Costa_Rica)
-Panama = country(**Panama)
-Nicaragua = country(**Nicaragua)
-Cuba = country(**Cuba)
-Haiti = country(**Haiti)
-Dominican_Republic = country(**Dominican_Republic)
-Colombia = country(**Colombia)
-Ecuador = country(**Ecuador)
-Peru = country(**Peru)
-Chile = country(**Chile)
-Argentina = country(**Argentina)
-Uruguay = country(**Uruguay)
-Paraguay = country(**Paraguay)
-Bolivia = country(**Bolivia)
-Brazil = country(**Brazil)
-Venezuela = country(**Venezuela)
-Chinese_Civil_War = country(**Chinese_Civil_War)
-
-
-# ### Build standard map
-
-# In[5]:
-
-
-def build_standard_map():
-    Panama.set_influence(1, 0)
-    Canada.set_influence(2, 0)
-    UK.set_influence(2, 0)
-    North_Korea.set_influence(0, 1)
-    East_Germany.set_influence(0, 3)
-    Finland.set_influence(0, 1)
-    Syria.set_influence(0, 1)
-    Israel.set_influence(1, 0)
-    Iraq.set_influence(0, 1)
-    Iran.set_influence(1, 0)
-    North_Korea.set_influence(0, 3)
-    South_Korea.set_influence(1, 0)
-    Japan.set_influence(1, 0)
-    Philippines.set_influence(1, 0)
-    Australia.set_influence(4, 0)
-    South_Africa.set_influence(1, 0)
-
-
-# In[6]:
-
-
-build_standard_map()
+from scoring_functionality import *
 
 
 # ### Create cards, hand, discard pile, draw pile, removed pile
 
-# In[7]:
+# In[2]:
 
 
 class card:
@@ -295,7 +43,7 @@ class card:
 
 # Let's create cards by index 1-110. Early war: 1-35, 103-106. Mid war: 36-81, 107-108. Late war: 82-102, 109-110. China card is card(6).
 
-# In[8]:
+# In[3]:
 
 
 all_cards = [card(i+1) for i in range(110)]
@@ -307,7 +55,7 @@ late_war_indices = [i+1 for i in range(82-1,102)]
 late_war_indices.extend([i+1 for i in range(109-1,110)])
 
 
-# In[9]:
+# In[4]:
 
 
 early_war_cards = [card(i) for i in early_war_indices]
@@ -315,7 +63,7 @@ mid_war_cards = [card(i) for i in mid_war_indices]
 late_war_cards = [card(i) for i in late_war_indices]
 
 
-# In[10]:
+# In[5]:
 
 
 us_hand = []
@@ -324,7 +72,7 @@ ussr_hand = []
 
 # ### Setup
 
-# In[11]:
+# In[6]:
 
 
 Arab_Israeli_War = card(**Arab_Israeli_War)
@@ -368,7 +116,7 @@ Vietnam_Revolts = card(**Vietnam_Revolts)
 Warsaw_Pact_Formed = card(**Warsaw_Pact_Formed)
 
 
-# In[ ]:
+# In[7]:
 
 
 '''
@@ -381,13 +129,13 @@ ussr_hand.extend([early_war_cards.pop() for i in range(8)])
 us_hand.extend([early_war_cards.pop() for i in range(8)])
 
 
-# In[ ]:
+# In[8]:
 
 
 ussr_hand
 
 
-# In[ ]:
+# In[9]:
 
 
 early_war_cards
@@ -399,16 +147,11 @@ early_war_cards
 late_war_cards
 
 
-# ### Create the turn order
-
-# ### Create space race track
-# 1. Create the actual track
-# 2. Create the buffs
+# ### Tracks
 
 # In[ ]:
 
 
-# TRACKS
 vp_track = 0 # positive for ussr
 turn_track = 1
 ar_track = 1 # increment by 0.5 for each side's action round
@@ -416,13 +159,56 @@ defcon_track = 5
 milops_track = [0, 0] # ussr first
 space_track = [0, 0] # 0 is start, 1 is earth satellite etc
 
-'''
-Need to add the additional functionality gained with being faster on the space race.
-'''
+
+# In[ ]:
+
+
+# to add game terminate functionality EndGame()
+def change_vp(n): # positive for ussr
+    global vp_track
+    vp_track += n
+    if vp_track >= 20:
+        print('USSR victory')
+        # EndGame()
+    if vp_track <= -20:
+        print('US victory')
+        # EndGame()
+
+def change_defcon(n):
+    global defcon_track
+    defcon_track += min(n, 5 - defcon_track)
+    if defcon_track < 2:
+        print('Game ended by thermonuclear war')
+        # EndGame()
+    
+
+
+# ### Create the turn order
+# 1. Increase DEFCON status
+# 2. Deal Cards
+# 3. Headline Phase
+# 4. Action Rounds (advance round marker)
+# 5. Check milops
+# 6. Check for held scoring card
+# 7. Flip China Card
+# 8. Advance turn marker
+# 9. Final scoring (end T10)
+
+# In[ ]:
+
+
+
+
+
+# ### Create space race track
+# 1. Create the buffs
+# 2. Need to add the additional functionality gained with being faster on the space race.
+
+# In[ ]:
+
 
 # SPACE (function)
 def space(side):
-    global vp_track
     x = 0
     if side == 'us':
         x = 1
@@ -435,93 +221,37 @@ def space(side):
         modifier = 1
     
     y = 1 - 2*x # multiplier for VPs - gives 1 for USSR and -1 for US
-    roll = np.random.randint(6)
-    if roll + 1 + modifier <= 3:
+    roll = np.random.randint(6) + 1
+    if roll + modifier <= 3:
         space_track[x] += 1
         print(f'Success with roll of {roll}.')
         
         if space_track[x] == 1:
             if space_track[1-x] < 1:
-                vp_track += 2*y
+                change_vp(2*y)
             else:
-                vp_track += y
+                change_vp(y)
         
         elif space_track[x] == 3:
             if space_track[1-x] < 3:
-                vp_track += 2*y
+                change_vp(2*y)
             
         elif space_track[x] == 5:
             if space_track[1-x] < 5:
-                vp_track += 3*y
+                change_vp(3*y)
             else:
-                vp_track += y
+                change_vp(y)
                 
         elif space_track[x] == 7:
             if space_track[1-x] < 7:
-                vp_track += 4*y
+                change_vp(4*y)
             else:
-                vp_track += 2*y
+                change_vp(2*y)
                 
         elif space_track[x] == 8:
             if space_track[1-x] < 8:
-                vp_track += 2*y
+                change_vp(2*y)
     
     else:
         print(f'Failure with roll of {roll}.')
-
-
-# ### CA Scoring
-
-# In[ ]:
-
-
-# CA SCORING TEST
-def ScoreCentralAmerica():
-    global vp_track
-    
-    CA = []
-    for x in list(all_countries.values()):
-        if x.region == "Central America":
-            CA.append(x)
-            
-    presence, domination, control = [1,3,5]
-
-    bg_count = [0,0] # USSR, US
-    country_count = [0,0]
-    bgs = 0
-
-    for x in CA:
-        bg_count[0] += (x.battleground and x.control == 'ussr')
-        bg_count[1] += (x.battleground and x.control == 'us')
-        country_count[0] += (x.control == 'ussr')
-        country_count[1] += (x.control == 'us')
-        bgs += x.battleground
-
-    swing = bg_count[0] - bg_count[1]
-
-    # presence
-    if country_count[0] > 0:
-        swing += presence
-    if country_count[1] > 0:
-        swing -= presence
-
-    # control
-    if bg_count[0] == bgs:
-        swing += control - presence
-    if bg_count[1] == bgs:
-        swing -= control - presence
-
-    # domination
-    if bg_count[0] < bgs and bg_count[0] > bg_count[1] and country_count[0] > country_count[1] and country_count[0] - bg_count[0] > 0:
-        swing += domination - presence
-    if bg_count[1] < bgs and bg_count[1] > bg_count[0] and country_count[1] > country_count[0] and country_count[1] - bg_count[1] > 0:
-        swing -= domination - presence
-
-    # adjacent
-    if Cuba.control == 'ussr':
-        swing += 1
-    if Mexico.control == 'ussr':
-        swing += 1
-
-    vp_track += swing
 
--- twilight_map.py
+++ twilight_map.py
@@ -1,3 +1,125 @@
+all_countries = dict()
+
+class country:
+    def __init__(self, country_name, country_index, adjacent_countries, us_influence, ussr_influence, region=0, stability=0, battleground=False, superpower=False, chinese_civil_war=False):
+        self.country_name = country_name
+        self.country_index = country_index
+        self.region = region
+        self.stability = stability
+        self.battleground = battleground
+        self.adjacent_countries = adjacent_countries
+        self.us_influence = us_influence
+        self.ussr_influence = ussr_influence
+        self.superpower = superpower
+        self.chinese_civil_war = chinese_civil_war
+        self.evaluate_control()
+        all_countries[country_name] = self
+
+    def evaluate_control(self):
+        if self.us_influence - self.ussr_influence >= self.stability:
+            self.control = 'us'
+        elif self.ussr_influence - self.us_influence >= self.stability:
+            self.control = 'ussr'
+        else:
+            self.control = 'none'
+
+    def __repr__(self):
+        if self.stability == 0:
+            return f'country({self.country_name}, adjacent = {self.adjacent_countries})'
+        else:
+            return f'country({self.country_name}, region = {self.region}, stability = {self.stability}, battleground = {self.battleground}, adjacent = {self.adjacent_countries}, us_inf = {self.us_influence}, ussr_inf = {self.ussr_influence}), control = {self.control}'
+
+    def set_influence(self, us_influence, ussr_influence):
+        if self.superpower == True:
+            raise ValueError('Cannot set influence on superpower!')
+        self.us_influence = us_influence
+        self.ussr_influence = ussr_influence
+        self.evaluate_control()
+
+    def reset_influence(self):
+        self.us_influence, self.ussr_influence = 0, 0
+        self.evaluate_control()
+
+    def adjust_influence(self, us_influence, ussr_influence):
+        self.us_influence += us_influence
+        self.ussr_influence += ussr_influence
+        self.evaluate_control()
+
+    def place_influence(self, side, effective_operations_points):
+        if side == 'ussr' and self.control == 'us':
+            # here we deduct 2 from effective_operations_points, to place 1 influence in the country, and then call the function again
+            if effective_operations_points >= 2:
+                self.adjust_influence(0, 1)
+            else:
+                raise ValueError('Not enough operations points!')
+            if effective_operations_points - 2 > 0:
+                self.place_influence(side, effective_operations_points - 2)
+        elif side == 'us' and self.control == 'ussr':
+            if effective_operations_points >= 2:
+                self.adjust_influence(1, 0)
+            else:
+                raise ValueError('Not enough operations points!')
+            if effective_operations_points - 2 > 0:
+                self.place_influence(side, effective_operations_points - 2)
+        else:
+            if side == 'us':
+                self.adjust_influence(effective_operations_points, 0)
+            elif side == 'ussr':
+                self.adjust_influence(0, effective_operations_points)
+            else:
+                raise ValueError("side must be 'us' or 'ussr'!")
+
+    def coup(self, side, effective_operations_points):
+        '''
+        TODO:
+        1. Prevent coup if no opposing influence in the country. I would prefer to write this in a way that prevents this from happening altogether, as opposed to throwing up an error if this is tried.
+        2. Prevent coup under DEFCON restrictions. Will write it in a style same as 1.
+        3. Prevent coup if there is no influence at all in the country.
+        4. Add military operations points.
+        5. Reduce DEFCON status level if self.battleground = True
+        '''
+
+        die_roll = np.random.randint(6) + 1
+        difference = die_roll + effective_operations_points - self.stability*2
+        if difference > 0:
+            if side == 'us':
+                # subtract from opposing first.. and then add to yours
+                if difference > self.ussr_influence:
+                    self.adjust_influence(difference - self.ussr_influence, 0)
+                self.adjust_influence(0, -min(difference, self.us_influence))
+
+            if side == 'ussr':
+                if difference > self.us_influence:
+                    self.adjust_influence(0, difference - self.us_influence)
+                self.adjust_influence(-min(difference, self.us_influence), 0)
+            print(f'Coup successful with roll of {die_roll}. Difference: {difference}')
+        else:
+            print(f'Coup failed with roll of {die_roll}')
+        self.evaluate_control()
+
+    def realignment(self):
+        '''
+        TODO:
+        1. Prevent realignment under DEFCON restrictions.
+        2. Prevent realignment if there is no influence at all in the country.
+        '''
+
+        modifier = 0
+        for adjacent_country in self.adjacent_countries:
+            modifier += ((all_countries[adjacent_country.country_name]).control == 'us')
+            modifier -= ((all_countries[adjacent_country.country_name]).control == 'ussr')
+        if self.us_influence - self.ussr_influence > 0:
+            modifier += 1
+        elif self.us_influence - self.ussr_influence < 0:
+            modifier -= 1
+        us_roll, ussr_roll = np.random.randint(6) + 1, np.random.randint(6) + 1
+        difference = us_roll - ussr_roll + modifier
+        if difference > 0:
+            self.adjust_influence(0, -min(difference, self.ussr_influence))
+        elif difference < 0:
+            self.adjust_influence(-min(-difference, self.us_influence), 0)
+        print(f'US rolled: {us_roll}, USSR rolled: {ussr_roll}, Modifer = {modifier}, Difference = {difference}')
+
 USSR = {
     "country_name": "USSR",
     "country_index": 1,
@@ -966,3 +1088,112 @@ Chinese_Civil_War = {
     "us_influence": 0,
     "ussr_influence": 0,
 }
+
+''' Creates entire map. '''
+USSR = country(**USSR)
+USA = country(**USA)
+Canada = country(**Canada)
+UK = country(**UK)
+Norway = country(**Norway)
+Sweden = country(**Sweden)
+Finland = country(**Finland)
+Denmark = country(**Denmark)
+Benelux = country(**Benelux)
+France = country(**France)
+Spain_Portugal = country(**Spain_Portugal)
+Italy = country(**Italy)
+Greece = country(**Greece)
+Austria = country(**Austria)
+West_Germany = country(**West_Germany)
+East_Germany = country(**East_Germany)
+Poland = country(**Poland)
+Czechoslovakia = country(**Czechoslovakia)
+Hungary = country(**Hungary)
+Yugoslavia = country(**Yugoslavia)
+Romania = country(**Romania)
+Bulgaria = country(**Bulgaria)
+Turkey = country(**Turkey)
+Libya = country(**Libya)
+Egypt = country(**Egypt)
+Israel = country(**Israel)
+Lebanon = country(**Lebanon)
+Syria = country(**Syria)
+Iraq = country(**Iraq)
+Iran = country(**Iran)
+Jordan = country(**Jordan)
+Gulf_States = country(**Gulf_States)
+Saudi_Arabia = country(**Saudi_Arabia)
+Afghanistan = country(**Afghanistan)
+Pakistan = country(**Pakistan)
+India = country(**India)
+Burma = country(**Burma)
+Laos_Cambodia = country(**Laos_Cambodia)
+Thailand = country(**Thailand)
+Vietnam = country(**Vietnam)
+Malaysia = country(**Malaysia)
+Australia = country(**Australia)
+Indonesia = country(**Indonesia)
+Philippines = country(**Philippines)
+Japan = country(**Japan)
+Taiwan = country(**Taiwan)
+South_Korea = country(**South_Korea)
+North_Korea = country(**North_Korea)
+Algeria = country(**Algeria)
+Morocco = country(**Morocco)
+Tunisia = country(**Tunisia)
+West_African_States = country(**West_African_States)
+Ivory_Coast = country(**Ivory_Coast)
+Saharan_States = country(**Saharan_States)
+Nigeria = country(**Nigeria)
+Cameroon = country(**Cameroon)
+Zaire = country(**Zaire)
+Angola = country(**Angola)
+South_Africa = country(**South_Africa)
+Botswana = country(**Botswana)
+Zimbabwe = country(**Zimbabwe)
+SE_African_States = country(**SE_African_States)
+Kenya = country(**Kenya)
+Somalia = country(**Somalia)
+Ethiopia = country(**Ethiopia)
+Sudan = country(**Sudan)
+Mexico = country(**Mexico)
+Guatemala = country(**Guatemala)
+El_Salvador = country(**El_Salvador)
+Honduras = country(**Honduras)
+Costa_Rica = country(**Costa_Rica)
+Panama = country(**Panama)
+Nicaragua = country(**Nicaragua)
+Cuba = country(**Cuba)
+Haiti = country(**Haiti)
+Dominican_Republic = country(**Dominican_Republic)
+Colombia = country(**Colombia)
+Ecuador = country(**Ecuador)
+Peru = country(**Peru)
+Chile = country(**Chile)
+Argentina = country(**Argentina)
+Uruguay = country(**Uruguay)
+Paraguay = country(**Paraguay)
+Bolivia = country(**Bolivia)
+Brazil = country(**Brazil)
+Venezuela = country(**Venezuela)
+Chinese_Civil_War = country(**Chinese_Civil_War)
+
+def build_standard_map():
+    Panama.set_influence(1, 0)
+    Canada.set_influence(2, 0)
+    UK.set_influence(2, 0)
+    North_Korea.set_influence(0, 1)
+    East_Germany.set_influence(0, 3)
+    Finland.set_influence(0, 1)
+    Syria.set_influence(0, 1)
+    Israel.set_influence(1, 0)
+    Iraq.set_influence(0, 1)
+    Iran.set_influence(1, 0)
+    North_Korea.set_influence(0, 3)
+    South_Korea.set_influence(1, 0)
+    Japan.set_influence(1, 0)
+    Philippines.set_influence(1, 0)
+    Australia.set_influence(4, 0)
+    South_Africa.set_influence(1, 0)
+
+build_standard_map()
