b46c4362b0da3963d5ecaa0a7683acdec7fd9758
Tym Teo <tym.teo.23@gmail.com>
2020-04-13 14:03:17 +0800

    Added draggable base image

--- front-end/vue/src/main.js
+++ front-end/vue/src/main.js
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueDragscroll from 'vue-dragscroll'
 
+Vue.use(VueDragscroll)
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount("#app");
--- front-end/vue/src/views/Game.vue
+++ front-end/vue/src/views/Game.vue
@@ -1,21 +1,40 @@
 <template>
-  <v-container class="my-5" fluid app>
-    <v-row justify="center">
-      <v-btn v-on:click="post">Click to POST</v-btn>
-    </v-row>
-  </v-container>
+    <div>
+        <v-container class="my-n3" fluid app>
+            <v-row class="img-wrapper" v-dragscroll="true">
+                <img src="@/assets/big.jpg" />
+            </v-row>
+            <v-row justify="center">
+                <v-btn v-on:click="post">Click to POST</v-btn>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Game",
-  methods: {
-    post() {
-      console.log("posting..");
+    name: "Game",
+    methods: {
+        post() {
+            console.log("posting..");
+        }
+    },
+    data() {
+        return {};
     }
-  },
-  data() {
-    return {};
-  }
 };
 </script>
+
+<style>
+.img-wrapper {
+    overflow: hidden;
+    height: 80vh;
+    background-color: black;
+    position: relative;
+}
+
+.img-wrapper > img {
+    height: 400%;
+    vertical-align: bottom;
+}
+</style>
